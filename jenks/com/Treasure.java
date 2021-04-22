bdb9ba4b177e9d4603bc5c184d9abca050d890a6
Matt Jenks <30731345+Saths0106@users.noreply.github.com>
2021-04-22 15:40:31 -0400

    Update Treasure.java

--- jenks/com/Treasure.java
+++ jenks/com/Treasure.java
@@ -1,13 +1,16 @@
 package CIS210M.jenks.com;
 /*Matthew Jenks
-4.21.2021
+4.22.2021
 getHired game to show employers what I know how to do.
 This class is meant to be the parent for showing inheritance
-I realize all classes extend object implicitly, this was the best way I thought of to show explicit inheritance*/
+I realize all classes extend object implicitly, this was the best way I thought of to show explicit inheritance
+small update to change abstract function from string to list for child class*/
+
+
+import java.util.List;
 
 public abstract class Treasure {
-    public abstract String generateTreasure();
-    public abstract String createLoot();
+    public abstract List generateTreasure(Character evilCreature);
     public abstract String sellTreasure();
     public abstract String buyTreasure();
     public abstract String pickUpLoot();
