b5dea58b81e933b12e53549858be71709a3b8e67
Matt Jenks <30731345+Saths0106@users.noreply.github.com>
2021-04-22 16:09:36 -0400

    Add files via upload

---
+++ CIS210M/Main.java
@@ -0,0 +1,15 @@
+package CIS210M;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        GCD gcd = new GCD();
+        gcd.run();
+
+
+
+    }
+}
