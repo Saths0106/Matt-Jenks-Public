846dc7a0c54afee2d986411b6491ac3210ec733e
Matt Jenks <30731345+Saths0106@users.noreply.github.com>
2021-04-22 16:13:21 -0400

    Update Main.java

--- CIS210M/Main.java
+++ CIS210M/Main.java
@@ -1,6 +1,9 @@
 package CIS210M;
 
 import java.io.IOException;
+/* comment created 4/22/2021
+Class project created December 2019. It works, and utilizes files so I thought it would be a good idea to upload. However, I have learned a lot since this was coded.
+*/
 
 public class Main {
 
