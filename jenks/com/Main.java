a961c16d75034781cd761150fe4b2ca921e3f020
Matt Jenks <30731345+Saths0106@users.noreply.github.com>
2021-04-23 16:07:07 -0400

    Update Main.java

--- jenks/com/Main.java
+++ jenks/com/Main.java
@@ -1,5 +1,74 @@
 package CIS210M.jenks.com;
+/*Matthew Jenkspackage CIS210M.jenks.com;
 /*Matthew Jenks
+4.23.2021
+getHired game to show employers what I know how to do.
+Classes are set up to explicitly try to show a Java capability
+I will update dates on classes as I write more code and update that class
+otherwise dates are accurate for the specific day I wrote that code
+Important to note too - I work full time and I'm a full time college student.
+I am also doing this game without psuedocode or a flow chart, I just wanted to put something out there
+Main updated to include JavaFX GUI, finally have a name for the game
+Also shows use of lambda
+*/
+
+
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class Main extends Application  {
+    private Scene startScene, mainScene;
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // I'm learning JavaFX, never used this. I've used Jframe, but this is learning as I go right now
+        primaryStage.setTitle("Luna's Realm");
+        ListView listView = new ListView();
+        listView.setMaxWidth(100);
+        listView.setMaxHeight(75);
+        listView.getItems().addAll("WARRIOR", "MAGE", "CLERIC");
+        Label label1 = new Label("Welcome to Luna's Realm! Start by building your character");
+        StackPane layout = new StackPane();
+        TextField textBox = new TextField();
+        textBox.setText("Please enter your username");
+        layout.setAlignment(textBox, Pos.BOTTOM_CENTER);
+        layout.setAlignment(label1, Pos.TOP_CENTER);
+        layout.getChildren().addAll(label1, listView, textBox);
+        startScene = new Scene(layout, 500, 500);
+        //these are examples of extremely helpful lambda
+        listView.setOnMouseClicked(e -> { //need to update classes to move information to mainScene, not IDE console
+            try {
+                new Gameplay(textBox.getText(), (String) listView.getSelectionModel().getSelectedItem());
+                mainScene = new Scene(Gameplay.populateSecondScreen(), 500, 500);
+                primaryStage.setScene(mainScene);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        primaryStage.setScene(startScene);
+        primaryStage.show();
+
+
+    }
+}
+
 4.23.2021
 getHired game to show employers what I know how to do.
 Classes are set up to explicitly try to show a Java capability
